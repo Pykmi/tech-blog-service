84d5315fdbfacf21e260b2a1cf081bb3380690a7
Tomi Kaistila <tomi.kaistila@tieto.com>
2019-01-21 09:30:11 +0200

    Removed admin routes and changed single content route to findOne middleware

--- routers/articles.js
+++ routers/articles.js
@@ -5,9 +5,9 @@ var express = require('express'),
 // create an express router
 var router = express.Router();
 
-// article/:id findById middleware
-router.use('/article/:id', (req, res, next) => {
-  models.articles.findById(req.params.id, (err, article) => {
+// article/:name findOne middleware
+router.use('/article/:name', (req, res, next) => {
+  models.articles.findOne({ url: req.params.name }, (err, article) => {
     if(err) {
       res.status(404).send({ error: 'no record found' });
       return;
@@ -19,54 +19,19 @@ router.use('/article/:id', (req, res, next) => {
       return;
     }
 
-    res.status(404).send({ error: 'no record found'});
+    res.status(404).send({ error: 'no record found' });
   });
 });
 
-// /articles route
+// base route for content
 router.route('/')
   .get(controller().get)
   .post(controller().post);
 
-// /articles/:id route
-router.route('/article/:id')
-  .delete((req, res) => {
-    req.article.remove((err) => {
-      err ? res.status(500).send() : res.status(200).send();
-    });
-  })
+// fetch individual article by name
+router.route('/article/:name')
   .get((req, res) => {
     res.json(req.article);
-  })
-  .patch((req, res) => {
-    req.body._id ? delete req.body._id : ''
-    req.body._v ? delete req.body._v : ''
-
-    for(let p in req.body) {
-      req.article[p] = req.body[p];
-    }
-
-    req.article.save((err) => {
-      err ? res.status(500).send() : res.status(200).send();
-    });
-  })
-  .put((req, res) => {
-    req.article.title = req.body.title;
-    req.article.author = req.body.author;
-    req.article.category = req.body.category;
-    req.article.tags = req.body.tags;
-    req.article.likes = req.body.likes;
-    req.article.url = req.body.url;
-    req.article.image = req.body.image;
-    req.article.smalltext = req.body.smalltext;
-    req.article.bodytext = req.body.bodytext;
-    req.article.public = req.body.public;
-    req.article.created_at = req.body.created_at;
-    req.article.modified_at = req.body.modified_at;
-
-    req.article.save((err) => {
-      err ? res.status(500).send() : res.status(200).send();
-    });
   });
 
 module.exports = router;
