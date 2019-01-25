bd2648a383a3f80a6b073ebeec58cd14da6b119d
Tomi Kaistila <tomi.kaistila@tieto.com>
2019-01-25 14:02:03 +0200

    Moved route paths into constants

--- routers/blog.js
+++ routers/blog.js
@@ -1,23 +1,31 @@
 var express = require('express'),
     models = require('../models'),
     controller = require('../controllers/articles'),
-    utils = require('../utils');
+    utils = require('../utils'),
+    route = require('./constants');
 
 // create an express router
 var router = express.Router();
 
-router.use('/article/:url', (req, res, next) => {
+// route middlewares
+router.use(route.BASE, (req, res, next) => {
   req.filters = utils.filters(req.params);
   next();
 });
 
-router.use('/category/:category', (req, res, next) => {
+router.use(route.BY_TITLE, (req, res, next) => {
   req.filters = utils.filters(req.params);
   next();
 });
 
-router.route('/').get(controller().fetch);
-router.route('/article/:url').get(controller().fetch);
-router.route('/category/:category').get(controller().fetch);
+router.use(route.BY_CATEGORY, (req, res, next) => {
+  req.filters = utils.filters(req.params);
+  next();
+});
+
+// route controllers
+router.route(route.BASE).get(controller().fetch);
+router.route(route.BY_TITLE).get(controller().fetch);
+router.route(route.BY_CATEGORY).get(controller().fetch);
 
 module.exports = router;
